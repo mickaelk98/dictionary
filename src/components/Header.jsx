4069652e4e27da7963a8af6aa05b8da2c8198256
mickael keita <keitamickael@gmail.com>
2023-10-11 14:53:04 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+import { LiaBookSolid } from "react-icons/lia";
+
+function Header() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <header className="max-w-5xl mx-auto flex justify-between py-2">
+      <div className="text-3xl">
+        <LiaBookSolid />
+      </div>
+      <div className="flex items-center">
+        <div className="border-r border-gray-400 mr-4">
+          <select name="" id="" className="mr-5">
+            <option value="serif">Serif</option>
+            <option value="sans-serif">Sans serif</option>
+          </select>
+        </div>
+        <div className="text-2xl cursor-pointer">
+          {darkTheme ? (
+            <div onClick={() => setDarkTheme(!darkTheme)}>
+              <BsSun />
+            </div>
+          ) : (
+            <div onClick={() => setDarkTheme(!darkTheme)}>
+              <BsMoon />
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
